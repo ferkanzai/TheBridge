d53289f7cc450a4d51c598222c0d267b3b378a05
Fernando Carmona <fernando.carmona@gruposantander.com>
2020-11-28 15:50:14 +0100

    changes in pokeapi

--- JS/EjercicioDOM_11142020_Pokeapi/index.js
+++ JS/EjercicioDOM_11142020_Pokeapi/index.js
@@ -22,65 +22,62 @@ const createP = (pkmnObj, arr, classNameDiv, classNameP, appendTo) => {
 
     typeDiv.append(p)
 
-    if(classNameP === 'type-name') {
+    if (classNameP === 'type-name') {
       pkmnObj[val.name].forEach((val) => {
         const p = document.createElement('p')
         p.innerText = capitalize(val)
-  
+
         typeDiv.append(p)
       })
     }
   })
 }
 
-const getPokemon = (id) => {
+let pokemon = {}
+
+const getPokemon = async (id) => {
   const pokeapiUrl = `https://pokeapi.co/api/v2/pokemon/${id}/`
-  return fetch(pokeapiUrl).then(res => res.json())
+  const res = await fetch(pokeapiUrl)
+  const data = await res.json()
+
+  pokemon[data.id] = {
+    name: data.name,
+    sprite: data.sprites.other['official-artwork'].front_default,
+    types: data.types.map((type) => type.type),
+    abilities: data.abilities.map((ability) => ability.ability)
+  }
+  console.log(pokemon)
 };
 
-const paintPokemon = (...fn) => {
-  
-  const pokemon = {}
+const getThree = (length) => [Math.floor(Math.random() * length), Math.floor(Math.random() * length), Math.floor(Math.random() * length)]
 
-  Promise.all(fn).then(data => {
+const paintPokemon = (id) => {
+
+  let pkmn = pokemon[`${id}`]
+  console.log(pkmn)
+
+  const pkmnTypesVal = Object.values(pkmn.types)
+  const pkmnAbilitiesVal = Object.values(pkmn.abilities)
+
+  const typesUrl = pkmnTypesVal.map((val, i, arr) => arr[i].url)
+
+  const urlPromises = typesUrl.map(url => {
+    return fetch(url).then(res => res.json())
+  })
+
+  Promise.all(urlPromises).then((data) => {
 
     data.forEach(d => {
-
-      pokemon[d.id] = {
-        name: d.name,
-        sprite: d.sprites.other['official-artwork'].front_default,
-        types: d.types.map((type) => type.type),
-        abilities: d.abilities.map((ability) => ability.ability)
+      const typeName = d.name
+      const arrPkmn = getThree(d.pokemon.length).reduce((acc, current, i) => [...acc, d.pokemon[current].pokemon.name], [])
+      pkmn = {
+        ...pkmn,
+        [typeName]: arrPkmn
       }
     })
+    const pkmnCard = createDiv('pkmn', cards)
 
-    const pokemonIds = Object.keys(pokemon)
-
-    pokemonIds.forEach((id) => {
-      let pkmn = pokemon[`${id}`]
-      
-      const pkmnTypesVal = Object.values(pkmn.types)
-      const pkmnAbilitiesVal = Object.values(pkmn.abilities)
-
-      const typesUrl = pkmnTypesVal.map((val, i, arr) => arr[i].url)
-
-      const urlPromises = typesUrl.map(url => {
-        return fetch(url).then(res => res.json())
-      })
-
-      Promise.all(urlPromises).then((data) => {
-
-        data.forEach(d => {
-          const typeName = d.name
-          const arrPkmn = d.pokemon.map((pkm, i) => i < 4 && i > 0 ? pkm.pokemon.name : false).filter(el => el)
-          pkmn = {
-            ...pkmn,
-            [typeName]: arrPkmn
-          }
-        })
-        const pkmnCard = createDiv('pkmn', cards)
-
-        pkmnCard.innerHTML = `
+    pkmnCard.innerHTML = `
         <h2>#${id} ${capitalize(pkmn.name)}</h2>
         <img src="${pkmn.sprite}" alt="${pkmn.name}">
         <h3 class='title'>Types:</h3>
@@ -88,17 +85,16 @@ const paintPokemon = (...fn) => {
         <h3 class='title'>Abilities:</h3>
         <div class='abilities' id='abilities-${pkmn.name}'></div>`
 
-        const pkmnTypes = document.querySelector(`#types-${pkmn.name}`)
-        createP(pkmn, pkmnTypesVal, 'type', 'type-name', pkmnTypes)
+    const pkmnTypes = document.querySelector(`#types-${pkmn.name}`)
+    createP(pkmn, pkmnTypesVal, 'type', 'type-name', pkmnTypes)
 
-        const pkmnAbilities = document.querySelector(`#abilities-${pkmn.name}`)
-        createP(pkmn, pkmnAbilitiesVal, 'ability', 'ability-name', pkmnAbilities)
-      })
-    })
-  });
+    const pkmnAbilities = document.querySelector(`#abilities-${pkmn.name}`)
+    createP(pkmn, pkmnAbilitiesVal, 'ability', 'ability-name', pkmnAbilities)
+  })
 }
 
-paintPokemon(getPokemon(1), getPokemon(4), getPokemon(7), getPokemon(25), getPokemon(133))
+getPokemon(1), getPokemon(4), getPokemon(7), getPokemon(25), getPokemon(133), getPokemon(77)
+Object.keys(pokemon).forEach(pkmn => paintPokemon(pkmn))
 
 const cards = document.querySelector('#cards')
 // const choosePkmnSelect = document.querySelector('#choose-pkmn')
@@ -113,9 +109,14 @@ const choosePkmnBtn = document.querySelector('#choose-pkmn-btn')
 choosePkmnBtn.addEventListener('click', () => {
   cards.innerHTML = ''
   pkmnId = document.querySelector('#choose-pkmn').value
-  if(pkmnId > 893 || pkmnId <= 0) {
+  if (pkmnId > 893 || pkmnId <= 0) {
     alert('Maximum ID is 893 and minimum 1')
   } else {
-    paintPokemon(getPokemon(pkmnId))
+    if (pokemon.hasOwnProperty(pkmnId)) {
+      paintPokemon(pkmnId)
+    } else {
+      getPokemon(pkmnId)
+      paintPokemon(pkmnId)
+    }
   }
 })
