61069711e0f04cbe04e6322545ae94a5cab946d9
Fernando Carmona <fernando.carmona@gruposantander.com>
2021-01-16 15:25:39 +0100

    GET landings by given recclass - /landings/recclass/:recclass

--- MongoDB/20210116_MongoAstronomyExercise/src/routes/landings.js
+++ MongoDB/20210116_MongoAstronomyExercise/src/routes/landings.js
@@ -1,32 +1,35 @@
 const router = require('express').Router();
 const mongoose = require('mongoose');
-const LandingModel = require('../models/Landing')
+const LandingModel = require('../models/Landing');
 
 const { createError } = require('../utils');
 
 router.get('/', async (req, res, next) => {
   try {
-    const { minimum_mass } = req.query
+    const { minimum_mass } = req.query;
 
-    if(!minimum_mass) {
-      const result = await LandingModel.find({}, { _id: 0 }).lean()
+    if (!minimum_mass) {
+      const result = await LandingModel.find({}, { _id: 0 }).lean();
       res.status(200).json({
         data: {
-          result
+          result,
         },
-        status: 'ok'
-      })
-      return
+        status: 'ok',
+      });
+      return;
     }
 
-    const result = await LandingModel.find({ $expr: { $gte: [{ $toDouble: "$mass" }, Number(minimum_mass)] } }, { _id: 0, name: 1, mass: 1 }).lean()
+    const result = await LandingModel.find(
+      { $expr: { $gte: [{ $toDouble: '$mass' }, Number(minimum_mass)] } },
+      { _id: 0, name: 1, mass: 1 }
+    ).lean();
 
     res.status(200).json({
       data: {
-        result
+        result,
       },
-      status: 'ok'
-    })
+      status: 'ok',
+    });
   } catch (error) {
     next(error.message);
   }
@@ -34,19 +37,44 @@ router.get('/', async (req, res, next) => {
 
 router.get('/mass/:mass', async (req, res, next) => {
   try {
-    const { mass } = req.params
+    const { mass } = req.params;
 
-    const result = await LandingModel.find({ $expr: { $eq: [{ $toDouble: "$mass" }, Number(mass)] } }, { _id: 0, name: 1, mass: 1 }).lean()
+    const result = await LandingModel.find(
+      { $expr: { $eq: [{ $toDouble: '$mass' }, Number(mass)] } },
+      { _id: 0, name: 1, mass: 1 }
+    ).lean();
 
     res.status(200).json({
       data: {
-        result
+        result,
       },
-      status: 'ok'
-    })
+      status: 'ok',
+    });
   } catch (error) {
-    next(error.message)
+    next(error.message);
   }
-})
+});
+
+router.get('/recclass/:recclass', async (req, res, next) => {
+  try {
+    const { recclass } = req.params;
+
+    console.log(recclass)
+
+    const result = await LandingModel.find(
+      { recclass: { $eq: recclass } },
+      { _id: 0, name: 1, recclass: 1 }
+    ).lean();
+
+    res.status(200).json({
+      data: {
+        result,
+      },
+      status: 'ok',
+    });
+  } catch (error) {
+    next(error.message);
+  }
+});
 
 module.exports = router;
