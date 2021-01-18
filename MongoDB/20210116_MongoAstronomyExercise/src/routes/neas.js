30206f9163b615379df3b3216793b41204136795
Fernando Carmona <fernando.carmona@gruposantander.com>
2021-01-18 19:36:26 +0100

    GET neas by orbit_class and paginated - /neas?orbit_class=&page=

--- MongoDB/20210116_MongoAstronomyExercise/src/routes/neas.js
+++ MongoDB/20210116_MongoAstronomyExercise/src/routes/neas.js
@@ -1,5 +1,58 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const NeaModel = require('../models/Neas');
 
-// Endpoints relativos a NEAs
+const itemsPerPage = 25;
 
-module.exports = router
+router.get('/', async (req, res, next) => {
+  try {
+    const { orbit_class } = req.query;
+    let { page } = req.query;
+    let skip = 0;
+
+    if (page) {
+      skip = (Number(page) - 1) * itemsPerPage;
+    } else {
+      page = 1;
+    }
+
+    let nextPage = Number(page) + 1
+
+    if (!orbit_class) {
+      const result = await NeaModel.find({}, { _id: 0 }).limit(itemsPerPage).skip(skip).lean();
+
+      if(result.length < itemsPerPage) {
+        nextPage = null
+      }
+
+      res.status(200).json({
+        data: {
+          result,
+        },
+        nextPage,
+        status: 'ok',
+      });
+      return;
+    }
+
+    const result = await NeaModel.find(
+      { $expr: { $eq: [{ $toLower: '$orbit_class' }, orbit_class.toLowerCase()] } },
+      { _id: 0, designation: 1, period_yr: 1 }
+    ).limit(itemsPerPage).skip(skip).lean();
+
+    if(result.length < itemsPerPage) {
+      nextPage = null
+    }
+
+    res.status(200).json({
+      data: {
+        result,
+      },
+      nextPage,
+      status: 'ok',
+    });
+  } catch (error) {
+    next(error.message);
+  }
+});
+
+module.exports = router;
