8d0ba26fbb037b97d0859cd0e8864dabca67d2ca
Fernando Carmona <fernando.carmona@gruposantander.com>
2021-01-19 20:48:56 +0100

    users route started

--- MongoDB/20210116_MongoAstronomyExercise/src/routes/users.js
+++ MongoDB/20210116_MongoAstronomyExercise/src/routes/users.js
@@ -1,5 +1,13 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const UserModel = require('../models/User');
 
+router.get('/', (req, res, next) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+});
 // Endpoints relativos a Users
 
-module.exports = router
+module.exports = router;
