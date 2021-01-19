b433c7fcd0463dbbd98cc7383ae6be09bbcb5435
Fernando Carmona <fernando.carmona@gruposantander.com>
2021-01-19 20:50:03 +0100

    camelize function created

--- MongoDB/20210116_MongoAstronomyExercise/src/utils/index.js
+++ MongoDB/20210116_MongoAstronomyExercise/src/utils/index.js
@@ -46,6 +46,28 @@ const checkDate = (date) => {
 
 const itemsPerPage = 20;
 
+const capitalize = (str) => str.slice(0, 1).toUpperCase() + str.slice(1);
+
+const camelize = (str) =>
+  str
+    .split(/[$-/:-?{-~!"^_`\[\]]/)
+    .map((word, index) => (index > 0 ? capitalize(word) : word))
+    .join('');
+
+// console.log(camelize('hola_que_tal'));
+
+const camelizeObject = (obj) => {
+  const objectFormatted = Object.keys(obj).reduce((acc, next, index) => {
+    const camelizedKey = camelize(next);
+
+    return {
+      ...acc,
+      [camelizedKey]: obj[next],
+    };
+  }, {});
+  return objectFormatted;
+};
+
 module.exports = {
   read,
   write,
@@ -54,4 +76,5 @@ module.exports = {
   getPlace,
   checkDate,
   itemsPerPage,
+  camelizeObject,
 };
