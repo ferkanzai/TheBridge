996bb0d89b1b8cf2d1d68c22c708245b6a86611f
Fernando Carmona <fernando.carmona@gruposantander.com>
2021-01-20 09:46:00 +0100

    POST new users to the guild - /guild

--- MongoDB/20210116_MongoAstronomyExercise/src/routes/users.js
+++ MongoDB/20210116_MongoAstronomyExercise/src/routes/users.js
@@ -1,13 +1,35 @@
 const router = require('express').Router();
 const UserModel = require('../models/User');
 
+UserModel.counterReset('affiliatedNumber', (err) => {});
+
 router.get('/', (req, res, next) => {
   try {
-    
   } catch (error) {
-    
+    next(error.message);
   }
 });
-// Endpoints relativos a Users
+
+router.post('/', async (req, res, next) => {
+  try {
+    const { name, nickname, occupation, birthdate } = req.body;
+
+    const result = await UserModel.create({
+      name,
+      nickname,
+      occupation,
+      birthdate,
+    });
+
+    res.status(200).json({
+      data: {
+        result,
+      },
+      status: 'ok',
+    });
+  } catch (error) {
+    next(error.message);
+  }
+});
 
 module.exports = router;
