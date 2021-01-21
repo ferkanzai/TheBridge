72d2a7cf689a7c9d722a9fc333e5fdbf5ba66432
Fernando Carmona <fernando.carmona@gruposantander.com>
2021-01-21 18:32:25 +0100

    GET users by affiliatedNumber - /guild/:affiliatedNumber

--- MongoDB/20210116_MongoAstronomyExercise/src/routes/users.js
+++ MongoDB/20210116_MongoAstronomyExercise/src/routes/users.js
@@ -1,10 +1,40 @@
 const router = require('express').Router();
 const UserModel = require('../models/User');
 
+const { calculateAge, createError } = require('../utils/');
+
 UserModel.counterReset('affiliatedNumber', (err) => {});
 
-router.get('/', (req, res, next) => {
+router.get('/:affiliatedNumber', async (req, res, next) => {
   try {
+    const { affiliatedNumber } = req.params;
+
+    const birthdate = await UserModel.find({ affiliatedNumber }, { birthdate: 1, _id: 0 });
+
+    const age = birthdate.length
+      ? calculateAge(birthdate[0].birthdate)
+      : createError('User not found', 404);
+
+    let result = await UserModel.find(
+      { affiliatedNumber },
+      {
+        name: 1,
+        occupation: 1,
+        affiliatedNumber: 1,
+        astronomicalPoints: 1,
+        affiliationDate: 1,
+        _id: 0,
+      }
+    ).lean();
+
+    result = { ...result[0], age };
+
+    res.status(200).json({
+      data: {
+        result,
+      },
+      status: 'ok',
+    });
   } catch (error) {
     next(error.message);
   }
